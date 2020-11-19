99973e282ec35ba264ffc3733cb08e878039211f
liv-soro <livia.soro@gmail.com>
2020-11-19 10:48:32 +0000

    Initial commit

---
+++ src/main/java/Servlet.java
@@ -0,0 +1,20 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import java.io.IOException;
+import java.util.stream.Collectors;
+
+
+@WebServlet(urlPatterns={"/patients"},loadOnStartup = 1)
+public class Servlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        resp.getWriter().write("Hey Emmy");
+        req.getServletPath();
+        //resp.getWriter().write(req.getServletPath());
+    }
+
+}
+
